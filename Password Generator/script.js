d1fe0ca7383f5505791b6ea5b884dc33abba5c85
Earth <37538704+Tree0@users.noreply.github.com>
2021-08-16 10:18:33 +0530

    Add files via upload

---
+++ Password Generator/script.js
@@ -0,0 +1,53 @@
+
+function randomNumberGenerator(min, max) {
+  return Math.floor(Math.random() * (max+1 - min) + min);
+}
+
+
+function passwordGenerator() {
+  let password ="";
+  let passLength = Number(document.querySelector('#passLength').value);
+  let upperCase = document.querySelector('#upperCase').checked;
+  let lowerCase = document.querySelector('#lowerCase').checked;
+  let num = document.querySelector('#num').checked;
+  let special = document.querySelector('#special').checked;
+
+  if(upperCase <= 0){
+    window.alert("Please tick at least one checkbox");
+    return;
+  }
+
+  for(let i = 0; i<passLength; i++) {
+
+    let randomPicker = randomNumberGenerator(0, 3);
+
+    if (upperCase && randomPicker === 0 ) {
+      password+=String.fromCharCode(randomNumberGenerator(65,90));
+    }
+
+    else if(lowerCase && randomPicker === 1){
+      password+=String.fromCharCode(randomNumberGenerator(97,122));
+    }
+
+    else if(num && randomPicker === 2){
+      password+=randomNumberGenerator(0,9);
+    }
+
+    else if(special && randomPicker === 3){
+      password+=String.fromCharCode(randomNumberGenerator(33,47));
+    }
+
+    else{
+      i--;
+    }
+
+
+  }
+
+    document.querySelector('#passGenerated').value = password;
+    console.log(password);
+
+}
+
+
+    document.querySelector('#generate').addEventListener('click', passwordGenerator);
